1961111f6097bc5bcbe0703d21cc1ea3307fc550
stickie <71910589+FieryIceStickie@users.noreply.github.com>
2025-06-01 02:45:33 +0800

    docs: Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'AMQCSLDB'
+project = 'AMQCSLdb'
 copyright = '2025, FieryIceStickie'
 author = 'FieryIceStickie'
 release = '1.0.6'
